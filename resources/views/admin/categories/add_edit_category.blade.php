62f36a8a9752a8d5a32ec691fc004cfd41b86f60
moshiur rahman <moshiur>
2020-12-30 13:36:00 +0600

    category add

--- resources/views/admin/categories/add_edit_category.blade.php
+++ resources/views/admin/categories/add_edit_category.blade.php
@@ -42,64 +42,48 @@
                     <input type="text" class="form-control" id="category_name" placeholder="Enter Category Name">
                 </div>
                 <div class="form-group">
-                  <label>Select Section</label>
-                  <select class="form-control select2" style="width: 100%;">
-                    <option value="">Select</option>
-                    @foreach ($getSections as $section)
-                    <option value="{{ $section->id }}">{{ $section->name }}</option>
-                    @endforeach
-                  </select>
+                    <label>Select Category Level</label>
+                    <select name="parent_id" class="form-control select2" style="width: 100%;">
+                      <option value="0">Main Category</option>
+
+                    </select>
                 </div>
               </div>
               <!-- /.col -->
               <div class="col-md-6">
                 <div class="form-group">
-                  <label>Multiple</label>
-                  <select class="select2" multiple="multiple" data-placeholder="Select a State" style="width: 100%;">
-                    <option>Alabama</option>
-                    <option>Alaska</option>
-                    <option>California</option>
-                    <option>Delaware</option>
-                    <option>Tennessee</option>
-                    <option>Texas</option>
-                    <option>Washington</option>
-                  </select>
-                </div>
-                <!-- /.form-group -->
-                <div class="form-group">
-                  <label>Disabled Result</label>
-                  <select class="form-control select2" style="width: 100%;">
-                    <option selected="selected">Alabama</option>
-                    <option>Alaska</option>
-                    <option disabled="disabled">California (disabled)</option>
-                    <option>Delaware</option>
-                    <option>Tennessee</option>
-                    <option>Texas</option>
-                    <option>Washington</option>
-                  </select>
-                </div>
-                <!-- /.form-group -->
+                    <label>Select Section</label>
+                    <select name="section_id" class="form-control select2" style="width: 100%;">
+                      <option value="">Select</option>
+                      @foreach ($getSections as $section)
+                      <option value="{{ $section->id }}">{{ $section->name }}</option>
+                      @endforeach
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputFile">Category Image</label>
+                    <div class="input-group">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="exampleInputFile">
+                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                      </div>
+                      <div class="input-group-append">
+                        <span class="input-group-text" id="">Upload</span>
+                      </div>
+                    </div>
+                  </div>
               </div>
-              <!-- /.col -->
             </div>
-            <!-- /.row -->
-
-            <h5>Custom Color Variants</h5>
             <div class="row">
               <div class="col-12 col-sm-6">
                 <div class="form-group">
-                  <label>Minimal (.select2-danger)</label>
-                  <select class="form-control select2 select2-danger" data-dropdown-css-class="select2-danger" style="width: 100%;">
-                    <option selected="selected">Alabama</option>
-                    <option>Alaska</option>
-                    <option>California</option>
-                    <option>Delaware</option>
-                    <option>Tennessee</option>
-                    <option>Texas</option>
-                    <option>Washington</option>
-                  </select>
+                    <label for="category_name">Category Discount</label>
+                    <input type="text" class="form-control" id="category_name" placeholder="Enter Category Name">
                 </div>
-                <!-- /.form-group -->
+                <div class="form-group">
+                    <label for="category_name">Category Url</label>
+                    <input type="text" class="form-control" id="category_name" placeholder="Enter Category Name">
+                </div>
               </div>
               <!-- /.col -->
               <div class="col-12 col-sm-6">
