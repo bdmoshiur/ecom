e086041aacdd3c1601e675dc7ee588421fe7ccab
Moshiur Rahman <moshiurcse888@gmail.com>
2022-09-24 10:10:21 +0600

    footer text change

--- resources/views/layouts/admin_layout/admin_footer.blade.php
+++ resources/views/layouts/admin_layout/admin_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2021
+    <strong>Copyright &copy; {{ date('Y') }}
         <div class="float-right d-none d-sm-inline-block">
             <b>Design & Develop-</b> Moshiur Rahman
         </div>
