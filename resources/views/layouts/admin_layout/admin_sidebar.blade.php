2e1438eb525a625326b6ed242b1db0c4b9dfa56b
moshiur <moshiurcse888@gmail.com>
2023-09-24 22:16:48 +0600

    update

--- resources/views/layouts/admin_layout/admin_sidebar.blade.php
+++ resources/views/layouts/admin_layout/admin_sidebar.blade.php
@@ -2,8 +2,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="#" class="brand-link">
-          <img src="" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-          {{-- <span class="brand-text font-weight-light">Website</span> --}}
+          <img src="https://img.freepik.com/premium-vector/abstract-modern-ecommerce-logo-design-colorful-gradient-happy-shopping-logo-template_467913-990.jpg" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+          <span class="brand-text font-weight-light">Website</span>
       </a>
 
       <!-- Sidebar -->
